f6791c52629aaff01512c1ea8522c0e48018abd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-04 09:06:59 +0000

    macchina: update 1.1.3 bottle.

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -10,6 +10,7 @@ class Macchina < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "da4b5c43e10971d0e93d12700a7396b90c1e7c32378d00dde8a7dbe91396a91b"
     sha256 cellar: :any_skip_relocation, catalina:      "55df09d90e450602340d08887667ed0941ae621652c2bd531bcbc751df1569af"
     sha256 cellar: :any_skip_relocation, mojave:        "cbacbfde75d9ce6c38757ba3f8ecdfc9c4344e3d9527c603f5c794fb8fdeef91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2610913dbbc2d5e3a40ae453d480747628d280707c11478ff7b59daa393b89fe"
   end
 
   depends_on "rust" => :build
