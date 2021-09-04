e845c602852f065ab22cf68af2b0009eac4eb35d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-04 16:45:01 +0000

    nauty: update 2.7r2 bottle.

--- Formula/nauty.rb
+++ Formula/nauty.rb
@@ -16,6 +16,7 @@ class Nauty < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c5d70f19f79bd5b9d3dde29d325c9dd26a936e3d2fc57dd9240e5e86bc928444"
     sha256 cellar: :any_skip_relocation, catalina:      "e600e32981977633cba530280714e35bfcc549601cf10ef7aedc655696af2929"
     sha256 cellar: :any_skip_relocation, mojave:        "d818e501a494790ce1a102bc181b818fa6ddaa420d8ea60298b0954c2273ab1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe3adac7f4d0a94f5f6b9afb2e498772b229df129ca73b22b827178286f78b93"
   end
 
   def install
