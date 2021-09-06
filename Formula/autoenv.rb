75bdc9171bd34fcc6442b30114358a6a6d3adccd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-06 17:25:06 +0000

    autoenv: add 0.3.0 bottle.

--- Formula/autoenv.rb
+++ Formula/autoenv.rb
@@ -6,6 +6,10 @@ class Autoenv < Formula
   license "MIT"
   head "https://github.com/hyperupcall/autoenv.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "879dc65b4f4740aed9cf1960c2b2da66f877f10c4ad774084373e36097d8efb0"
+  end
+
   def install
     prefix.install "activate.sh"
   end
