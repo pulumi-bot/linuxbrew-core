68c3b3a90e78debb8676be5d39cb7314dc7221c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-06 06:41:03 +0000

    wayland-protocols: add 1.22 bottle.

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -5,6 +5,10 @@ class WaylandProtocols < Formula
   sha256 "96e7cf03524995a47028236c6d6141c874e693cb80c0be8dabe15455cdd5a5a7"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "97dc1b8c59b0e36bc6a3f78863de15a5e675f6231d371f6ca98d2ee21ee6cf4e"
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "wayland" => :build
   depends_on :linux
