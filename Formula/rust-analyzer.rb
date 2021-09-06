d465530f46ed9accc133306e7b0b5defc362cc45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-06 17:42:20 +0000

    rust-analyzer: update 2021-09-06 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -12,6 +12,7 @@ class RustAnalyzer < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "32c0e6dec4b25082e30cd01225fd4f50c853e31a0f78bce1e01d08063a09a64b"
     sha256 cellar: :any_skip_relocation, catalina:      "497254e6af6c94e5079aa39f267bf7f6db41fb1c713d270beafed06b6065910e"
     sha256 cellar: :any_skip_relocation, mojave:        "5e8263b373343154f421cfbe5a0841e664a2544653b19375584bc33015a74ae3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4a8e7fa9c2b11ca850160f62d0842e4be961235111f4427fdeb43504a0017f3"
   end
 
   depends_on "rust" => :build
