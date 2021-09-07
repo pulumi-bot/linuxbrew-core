dfc696858684909c938d70c002927cb30a18b9d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-07 19:46:05 +0000

    groovysdk: update 3.0.9 bottle.

--- Formula/groovysdk.rb
+++ Formula/groovysdk.rb
@@ -15,6 +15,7 @@ class Groovysdk < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
     sha256 cellar: :any_skip_relocation, catalina:      "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
     sha256 cellar: :any_skip_relocation, mojave:        "a0d0761356557724fede393dc0168097b07340a626875936dfca34dacc5a9f71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3103793592e4d8c6392f5bf218e91698ffd2ad53a99002963ca4b8dd3c03fa7"
   end
 
   depends_on "openjdk"
