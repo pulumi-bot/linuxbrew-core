b429a0399ab8e5e40d5f4453427bb78611cfba36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-07 19:44:12 +0000

    dfu-util: update 0.11 bottle.

--- Formula/dfu-util.rb
+++ Formula/dfu-util.rb
@@ -10,6 +10,7 @@ class DfuUtil < Formula
     sha256 cellar: :any,                 big_sur:       "b970a649e90f3e080af2143e8479e0616959e35650defea16b96288c4af011dc"
     sha256 cellar: :any,                 catalina:      "5a5d86794a00b9559ffc819715c297da4f477296d20a92c804aefc426795d0b0"
     sha256 cellar: :any,                 mojave:        "1ded847895f4d2a86a4a7754fa711014d09c334044ccc03aa97d89059ae58604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c26d1ebc67dd60446845d51e3ff92bcded0ae223852dc104e897c8c3423f71"
   end
 
   head do
