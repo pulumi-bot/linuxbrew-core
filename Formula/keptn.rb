24fd137a1ec7b9cb6d9acfcecb5128b3f4bfef43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-07 16:37:35 +0000

    keptn: update 0.9.0 bottle.

--- Formula/keptn.rb
+++ Formula/keptn.rb
@@ -10,6 +10,7 @@ class Keptn < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "fddf741b6a464608285a10972531652bdc5dc86e9e0aefb750aa373445da05c2"
     sha256 cellar: :any_skip_relocation, catalina:      "9c50b1d8a015599aceaa738f88c5d39d97d52604fb5753761d01001d3ce432df"
     sha256 cellar: :any_skip_relocation, mojave:        "3e930645c90793a6678b2e3c4e5164f96381ae29757d618aa4bfdb889b65590a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ba4311c3e23b7b619bbf42be97f47f114689311562db80e78abe5e2baef0738"
   end
 
   depends_on "go" => :build
