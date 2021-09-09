30f7fdc917666fe69753be97153e6339dfb2db90
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-09 21:10:24 +0000

    prettier: update 2.4.0 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -13,6 +13,7 @@ class Prettier < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "dba56c931ade87ee538b304a62d9cafe768b5cd41c3d39f4996a02612dac5eeb"
     sha256 cellar: :any_skip_relocation, catalina:      "dba56c931ade87ee538b304a62d9cafe768b5cd41c3d39f4996a02612dac5eeb"
     sha256 cellar: :any_skip_relocation, mojave:        "dba56c931ade87ee538b304a62d9cafe768b5cd41c3d39f4996a02612dac5eeb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2cb09c254b3de8bf4244334afd36cbd3fc12a793a2888b508c7c69e0cf82b0e"
   end
 
   depends_on "node"
