d584e2b9ad90cad02ae787fe3605f13eedcd3ab5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-09 21:13:01 +0000

    jenkins: add 2.311 bottle.

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -10,6 +10,10 @@ class Jenkins < Formula
     regex(%r{href=.*?/war/v?(\d+(?:\.\d+)+)/jenkins\.war}i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a2d3c82ce97f28e1ef4b9c80f4a93a9a5b9ac2052632294f2d0d6d5f86b49d96"
+  end
+
   head do
     url "https://github.com/jenkinsci/jenkins.git"
     depends_on "maven" => :build
