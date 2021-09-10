02dc6e0e49781dee4bc046f2528c9442d6a36115
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-10 05:28:36 +0000

    wiremock-standalone: add 2.31.0 bottle.

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -6,6 +6,10 @@ class WiremockStandalone < Formula
   license "Apache-2.0"
   head "https://github.com/tomakehurst/wiremock.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b67b5dfa7adcb90b77d03af24a50bb2ff12f1007afd012b9936db3752e3897ce"
+  end
+
   depends_on "openjdk"
 
   def install
