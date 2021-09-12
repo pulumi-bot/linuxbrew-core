ba261744c42aedb614908ae29ad2a0d7de256b34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-12 08:16:14 +0000

    buildifier: update 4.2.1 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -11,6 +11,7 @@ class Buildifier < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1155f17cc3bb102deccaa9e8ee5bff1a8742984d5901d809613c26e9a439123c"
     sha256 cellar: :any_skip_relocation, catalina:      "1155f17cc3bb102deccaa9e8ee5bff1a8742984d5901d809613c26e9a439123c"
     sha256 cellar: :any_skip_relocation, mojave:        "1155f17cc3bb102deccaa9e8ee5bff1a8742984d5901d809613c26e9a439123c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2de6a7ea54e77e9e349f2198009b3b6a36d5bc13dc843b2390102ba68df5fa07"
   end
 
   depends_on "go" => :build
