c1f8cd697c052051183e977cd865de36eab19ef5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 11:37:22 +0000

    hg-fast-export: add 210917 bottle.

--- Formula/hg-fast-export.rb
+++ Formula/hg-fast-export.rb
@@ -7,6 +7,10 @@ class HgFastExport < Formula
   sha256 "168f51301f01b4a2572bb00dd9474ee9a50b85f24aa5ed5a7b8abb12896d951a"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f6532a51ead9d46a0ae6f957ae12f62467d265a741b2cf4be23b0808ed2be1fe"
+  end
+
   depends_on "mercurial"
   depends_on "python@3.9"
 
