2964f6764ed69a91b4792966b351d0fc2e76669a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 17:33:16 +0000

    libuninameslist: update 20210917 bottle.

--- Formula/libuninameslist.rb
+++ Formula/libuninameslist.rb
@@ -16,6 +16,7 @@ class Libuninameslist < Formula
     sha256 cellar: :any,                 big_sur:       "934119708939c95e650ac5b71d91d641271cd29d19531b01b10cffaeef5540b1"
     sha256 cellar: :any,                 catalina:      "bae124332a8758ab65c61230180c586c730b852d853c3fcf0705cd3e18a7e6ed"
     sha256 cellar: :any,                 mojave:        "be2893d87050b61ecfd82425a034873f8decb5d6d541ca604d0cc4f54dedd1dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c127ca07c9d350ba1fec668a8bce88ee221e5dd47350ea67218793297990c752"
   end
 
   head do
