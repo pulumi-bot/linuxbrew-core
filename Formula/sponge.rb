e554f0715840aac66051c2ae4b2f1fc9f2da8a75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 20:33:25 +0000

    sponge: update 0.66 bottle.

--- Formula/sponge.rb
+++ Formula/sponge.rb
@@ -15,6 +15,7 @@ class Sponge < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1e1d78aae23a4a4468c44eaa1941ebda24cfeb739044e1b47e4b28d1e1471ca6"
     sha256 cellar: :any_skip_relocation, catalina:      "81ba802eac1d5423220408709176ae92641b156b661b1f6821c91a18d7f20bed"
     sha256 cellar: :any_skip_relocation, mojave:        "f5580c82edb4d175d1285ff3320606785820b65e7c04e912a15b6405d34fc303"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f179a9c9f9215497e21595315fb13dd834caab7cd750ef77ec2c2e9626ff37e"
   end
 
   conflicts_with "moreutils", because: "both install a `sponge` executable"
