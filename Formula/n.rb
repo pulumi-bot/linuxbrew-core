54c12bd4a8a02dac2eda296adbbcbe8ffef81b7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-19 20:32:40 +0000

    n: update 7.4.1 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -11,6 +11,7 @@ class N < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1ccc72bec6169bc2a660c7100438132266e0bfcf441775306acac3d6322351ba"
     sha256 cellar: :any_skip_relocation, catalina:      "1ccc72bec6169bc2a660c7100438132266e0bfcf441775306acac3d6322351ba"
     sha256 cellar: :any_skip_relocation, mojave:        "1ccc72bec6169bc2a660c7100438132266e0bfcf441775306acac3d6322351ba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d98eb65e495bdb70e93f83b38250b481154e32aca63d976962159af3c44cec2"
   end
 
   def install
