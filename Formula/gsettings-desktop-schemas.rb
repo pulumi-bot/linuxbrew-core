f9b44557cb48ccb11febf5be2855bb086a165238
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-20 19:31:54 +0000

    gsettings-desktop-schemas: update 41.0 bottle.

--- Formula/gsettings-desktop-schemas.rb
+++ Formula/gsettings-desktop-schemas.rb
@@ -10,6 +10,7 @@ class GsettingsDesktopSchemas < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
     sha256 cellar: :any_skip_relocation, catalina:      "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
     sha256 cellar: :any_skip_relocation, mojave:        "57cdbf1950a1dea3008b6f4c8ed0a1d7d38a5a9e8cedfa31ca3ac5b4dfbead66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08f6428fc391dda01e161b14a0bd183d340b78cf5cb85a9e66b4ba33be790fa8"
   end
 
   depends_on "gobject-introspection" => :build
