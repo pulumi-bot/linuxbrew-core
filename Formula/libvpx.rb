8811053766f0bc53c1baf349955743cd16a61f1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-20 19:29:43 +0000

    libvpx: update 1.10.0_1 bottle.

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -13,6 +13,7 @@ class Libvpx < Formula
     sha256 cellar: :any,                 big_sur:       "a5c097e3c902b9c1bdfaf6c1100e01366d6537b1983f0bc4075e3a642c8ffab9"
     sha256 cellar: :any,                 catalina:      "ee2c55fcd7d7143a74171c42d227e63c383f46bfe78d2d8fac4dc45cc6a1e8e0"
     sha256 cellar: :any,                 mojave:        "3d578a1b4e7a4cdb73c07b3529b9f90c4cfa6583386035400be5cf85448e625d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8597613378a9e3e33d79dc63db9029edf7376780152eaf55e9ed35581670f2e6"
   end
 
   depends_on "yasm" => :build
