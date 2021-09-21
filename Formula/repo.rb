493b7487978e32d687e077ab3dd93a3edbca3932
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 20:05:37 +0000

    repo: add 2.16.8 bottle.

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -9,6 +9,10 @@ class Repo < Formula
   license "Apache-2.0"
   version_scheme 1
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "83e0d9547697d414580aec9fc2df6cac89d8699d36c73d86cdf9490bb7675e70"
+  end
+
   depends_on "python@3.9"
 
   def install
