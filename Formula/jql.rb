5f78d0bb6f5060a111924a11f99fd823826901fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 20:11:59 +0000

    jql: update 3.0.0 bottle.

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -11,6 +11,7 @@ class Jql < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "f30ad627f9e08c1e922ec14cf8d6baeb23d58dfee3e2c23862ef2b973fba0673"
     sha256 cellar: :any_skip_relocation, catalina:      "00a092b02141bb0fa453a8110da879db8704b07df2dce03d9bd70faa1a6c13fa"
     sha256 cellar: :any_skip_relocation, mojave:        "5153448e6d1bdd2de3f5c2b3460dd24268523d16e0b84ee258f4c38a789a0f51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f6d65ce78f39335751a5f5193995824e381a9691f7a472b32a2acb8a658f1b1"
   end
 
   depends_on "rust" => :build
