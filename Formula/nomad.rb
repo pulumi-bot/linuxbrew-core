0395c75f706c2c9ed2f44adfa07cb5a60fcc7051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 20:07:40 +0000

    nomad: update 1.1.5 bottle.

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -16,6 +16,7 @@ class Nomad < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "5a2b7842b522a4bde03bf7c15de5cb6f007b881226f73d41dbe152ad1d6f6bf9"
     sha256 cellar: :any_skip_relocation, catalina:      "2a8b0b54f049f2bb75536d6c1215ac5f9b183aa926afa66cb78eae390f08a4b7"
     sha256 cellar: :any_skip_relocation, mojave:        "9bd4472c8ded1863bc7d40df4232ea92f8970a14b2425ae679af013cf18aba77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cec0d18c2c691ef41acbd87dc8ac4788f64b4931480494acae64a2c1bc724e00"
   end
 
   depends_on "go" => :build
