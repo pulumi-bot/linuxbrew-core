05f5306b3c28ab4df1ffcf6507099c97bdd1ede3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 20:08:19 +0000

    cli11: add 2.1.0 bottle.

--- Formula/cli11.rb
+++ Formula/cli11.rb
@@ -6,6 +6,10 @@ class Cli11 < Formula
   license "BSD-3-Clause"
   head "https://github.com/CLIUtils/CLI11.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "14e40203b7a5a2a7aba74846647ed01780ba2ebf97fbaeb78f9a82ec2a12a543"
+  end
+
   depends_on "cmake" => :build
 
   def install
