b7149f1cc9f8806ba70f90f03a8155fc92017386
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-23 19:42:13 +0000

    nats-server: update 2.6.0 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -11,6 +11,7 @@ class NatsServer < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9b4afd53a631e88a952b7035ed76c084c42b2e7088526d06cc9432a294455f2a"
     sha256 cellar: :any_skip_relocation, catalina:      "79f78142a2457e399fa2d9f29800f7a8e57cc12ff5bcbbd68920dd41379c316e"
     sha256 cellar: :any_skip_relocation, mojave:        "70e96b75c6ec23de0196a9c1d95df5846fc11a09f453c028a8d8b81b81c65c5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d70ed49dc85be6567d88a28e708a27378acf3d5f6ea65701287a84d2072038c"
   end
 
   depends_on "go" => :build
