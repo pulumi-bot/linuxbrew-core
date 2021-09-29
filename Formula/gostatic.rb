d29f7015a72add0c1dc3a477f30bf9f23bb2efe0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-29 11:41:49 +0000

    gostatic: update 2.30 bottle.

--- Formula/gostatic.rb
+++ Formula/gostatic.rb
@@ -11,6 +11,7 @@ class Gostatic < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b1b74b8e646a07562fb4201215a55cdca9975d189b669b0a1a92ddd5915c9bf7"
     sha256 cellar: :any_skip_relocation, catalina:      "4db3ec282a190b3fe7b79f37cce04dd5b9913924a1c539563c5ee42b89c480c4"
     sha256 cellar: :any_skip_relocation, mojave:        "0bf75b7c812f2ef9bf9ecd78ea2d74c5fbb93bb7edf8502a796271fe312c21ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "489dcb53c5a2e1f96136a17602b91b3223428b3099a94123d577f7c4198b09be"
   end
 
   depends_on "go" => :build
