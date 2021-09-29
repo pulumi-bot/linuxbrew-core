e74fb255cd4bc17d4062769d582f6afdfe642a38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-29 17:51:17 +0000

    juju: update 2.9.15 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -18,6 +18,7 @@ class Juju < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c66d25f0b2ff66ae22e601afc6a29d663077b0716f3af0fe43a7581a4d2b8cde"
     sha256 cellar: :any_skip_relocation, catalina:      "501c989c4cbdb5a24d3e8c8d6c1838b69330c07266c7d764712f1e6807dce614"
     sha256 cellar: :any_skip_relocation, mojave:        "7fca8357223fc1b86239c10d451d3c61feb27620b49beb0e0392ba474cfe0ff6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "805cf11a774f6dc54ce31b285f2cf124c62be3b034a40679cdfaa20e7b0a74d7"
   end
 
   depends_on "go" => :build
