f657145312cd5ec3b121bcb1817ec738e33d87c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-30 19:12:13 +0000

    chezmoi: update 2.5.1 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -12,6 +12,7 @@ class Chezmoi < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "58b6452ed82834209756faa95e804b63a3da7d28cc4ba5cf1ffe6202c714593d"
     sha256 cellar: :any_skip_relocation, catalina:      "d23d6d2c3f464c70aefa11e716c7fa7dbd87de24a8ff19acef9f69571b4db4ed"
     sha256 cellar: :any_skip_relocation, mojave:        "8aad68e69bd80c55bf3c5fe02e919c9d5c027382d39ae279ee6bc02675a800da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e764144e83ff9991d3ae1a82d773e06ad7ed82b60cf943c7611af0018cae37bf"
   end
 
   depends_on "go" => :build
