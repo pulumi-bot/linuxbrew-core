6daf773d5779b0532647c75d65e8935bc1542c97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-30 19:56:33 +0000

    tile38: update 1.26.0 bottle.

--- Formula/tile38.rb
+++ Formula/tile38.rb
@@ -12,6 +12,7 @@ class Tile38 < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d8985f5f1746a6c2fc64a36adb6b1d3fc54d2584f04222a598010d16f7ff7a2a"
     sha256 cellar: :any_skip_relocation, catalina:      "ed03a353784f0b9a35de2660c955fd7baded73e6d3916a8f4fea232a5a5767e6"
     sha256 cellar: :any_skip_relocation, mojave:        "e39659e21794a852634f53fe26733d8c73599ffdeea77f811d76e0fe483191ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d69af284cda466f78e9fe1f258bf847b75d88dccea4c847b4128263e0eb5ad92"
   end
 
   depends_on "go" => :build
