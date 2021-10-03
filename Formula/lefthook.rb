82d8144cb9df14640bedd321d286215a8f88e762
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-03 20:14:26 +0000

    lefthook: update 0.7.7 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -11,6 +11,7 @@ class Lefthook < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "dce9b5d997e2543c0226e2855cced2778fea34a0454a86ba13e38bf27a0482bd"
     sha256 cellar: :any_skip_relocation, catalina:      "026c220ab4f8e98fc27be92035a170d004711e66e3e1335f6268fb3a659ac811"
     sha256 cellar: :any_skip_relocation, mojave:        "163adf2c067d85e44cce71682ce2f94421af706017bf8601cf4c88d262a67e41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5d01af7de4480e289f5d132d0103610aa4c787e3bdf17ed892d2b860362dd4b"
   end
 
   depends_on "go" => :build
