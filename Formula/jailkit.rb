a02d4a1c67c4217f824f1f8e9038c9e9ed93f438
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-10-04 18:51:31 +0000

    jailkit: update 2.23 bottle.

--- Formula/jailkit.rb
+++ Formula/jailkit.rb
@@ -15,6 +15,7 @@ class Jailkit < Formula
     sha256 big_sur:       "8e0b17405a1dabdc6c1869ccb304ded93a02f56d0b98a071b6616552c8b1dba6"
     sha256 catalina:      "f194ba5166062f17c00e48c076828da5c8fa5d97d35f20015d21fb6aea72febc"
     sha256 mojave:        "40c10ff51c0d1af469f749a764a3e5a9a1c0aa25c2174b40ae19671724b17895"
+    sha256 x86_64_linux:  "82d802ad818f3d5403480c888085bb5ce7c3e44f1bae84a48a0236fbbf9d8f18"
   end
 
   depends_on "python@3.9"
