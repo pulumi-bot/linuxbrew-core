b77be16e752e069cbaa46f4facc5f66a8ff1439b
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-10-06 07:59:08 +0000

    libnghttp2: update 1.45.1 bottle.

--- Formula/libnghttp2.rb
+++ Formula/libnghttp2.rb
@@ -12,6 +12,7 @@ class Libnghttp2 < Formula
     sha256 cellar: :any,                 big_sur:       "6edccdb5f700fa3602caa4ed902c18cdab02e64f33bdaf318a867b30b972a472"
     sha256 cellar: :any,                 catalina:      "f9d462cb615767a7af790b54af5f377ab80c0a993b7938c8743118db52822984"
     sha256 cellar: :any,                 mojave:        "58aa1edc1bc6d578976ba92fd7356628f660a5a1dcdad72da9d9e6c63f66dd8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88cb5305619855077a4f21256f150de396de0d9e3616c52b85bd39ffff1e9344"
   end
 
   head do
