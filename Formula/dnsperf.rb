7f8c9c54ff10a2892539dcb26a46123c7a36e059
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-10-06 08:24:54 +0000

    dnsperf: update 2.7.1_2 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -16,6 +16,7 @@ class Dnsperf < Formula
     sha256 cellar: :any,                 big_sur:       "33f81adab77f7d988a927144bdf77f5090d3489e0d3ef812f3b273c74cd07e25"
     sha256 cellar: :any,                 catalina:      "88b0ae4ee57e39b8e87f97d5b1aac1c12b5f44e744aad1316b45619781e25b34"
     sha256 cellar: :any,                 mojave:        "4d9debb3fb6b0a43620a205abf27d083d6b731e355afcb8c55f23812347f7aa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cae9a4ea9e132cc17ab2c378c5e12b7c1159edf536dc839438afdbffb43bb858"
   end
 
   depends_on "pkg-config" => :build
