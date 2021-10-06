8cfafb0d33759bc51cc0a6069ed99da939dd5b0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-06 19:43:13 +0000

    scons: update 4.2.0_1 bottle.

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -13,6 +13,7 @@ class Scons < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a5e7cc6c2d5dc05d1cd02ca3927425ccaf583cbbe2a8d97760bfa45d8479bd80"
     sha256 cellar: :any_skip_relocation, catalina:      "a5e7cc6c2d5dc05d1cd02ca3927425ccaf583cbbe2a8d97760bfa45d8479bd80"
     sha256 cellar: :any_skip_relocation, mojave:        "a5e7cc6c2d5dc05d1cd02ca3927425ccaf583cbbe2a8d97760bfa45d8479bd80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ea01cfe768ad1e59ec0c1dc6e777c6ab36fb821ca553bf5e54d9f74dd35b07f"
   end
 
   depends_on "python@3.10"
