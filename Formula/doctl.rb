ad11735fe63283399dab85bf6017035cbd6c0c2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-07 20:56:44 +0000

    doctl: update 1.65.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -11,6 +11,7 @@ class Doctl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e86f423a95e66ac52f95260c6ee5f31de2d14407aa7228e60d673bef93ce64f9"
     sha256 cellar: :any_skip_relocation, catalina:      "367d604509d4821204ffc997a09f04a24bd646446d25486374cb156c41deabd1"
     sha256 cellar: :any_skip_relocation, mojave:        "7bdd20fb58a102d1035ea5ccff9514b5f32e3281ac8ae76caf895b5ddd6e9a0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f085be99de8bb758f92368c5829e1c515e89e0186d0f55186fee4310f9409f53"
   end
 
   depends_on "go" => :build
