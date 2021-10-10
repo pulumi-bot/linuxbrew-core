360835ed67b68be75d2a3ba7f69c425284ec32be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 17:42:30 +0000

    log4cxx: update 0.12.1 bottle.

--- Formula/log4cxx.rb
+++ Formula/log4cxx.rb
@@ -11,6 +11,7 @@ class Log4cxx < Formula
     sha256 cellar: :any,                 big_sur:       "6d1b6f87ea1ffcc38069798e5150ad6523e7d64d2682b1c4afc2e4bd1c5e9294"
     sha256 cellar: :any,                 catalina:      "8f33426b24d1d711a72ce4ee1328a5c73607d5f1755c8055361aa9f74dbbebae"
     sha256 cellar: :any,                 mojave:        "14367506f0070f9142b47ca2de95101440ff0b9a6a1ea00dc6095d338c0f2b04"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e640dce6a616b1e0f083069b51f77da795916fde0e17da4a9d1a5a16bbb652df"
   end
 
   depends_on "cmake" => :build
