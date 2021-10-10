cf0307866d169e5086c608bd5e09eaffe92deb86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 13:04:02 +0000

    envconsul: update 0.12.0 bottle.

--- Formula/envconsul.rb
+++ Formula/envconsul.rb
@@ -10,6 +10,7 @@ class Envconsul < Formula
     sha256 cellar: :any_skip_relocation, big_sur:      "f840cd513aa64badff578d8edbd3d08d049f54e97c828e9c46b1dcf9ffb1c1fe"
     sha256 cellar: :any_skip_relocation, catalina:     "3a7f5c2bb6fadca3f7c17b66ef1500656811f9f929cbd1cf42a7994a58d62256"
     sha256 cellar: :any_skip_relocation, mojave:       "9602825e9692a3080ce96532bcf9c530626335824f84afdeace705125989093e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "40c7e21d5e090cffc3bb14106a4b51e45a7ec1bd8c5168aa7d5d0cf0509d82e1"
   end
 
   depends_on "go" => :build
