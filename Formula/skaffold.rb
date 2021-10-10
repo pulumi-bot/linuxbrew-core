de639c15c3777b3821e32ab558fef87349079e46
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-10 13:06:43 +0000

    skaffold: update 1.33.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -12,6 +12,7 @@ class Skaffold < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1d9b4729d15d2fab7c71c7849cf08ec7e4ff0ad3a731a89abff659b217ae271f"
     sha256 cellar: :any_skip_relocation, catalina:      "73fb0cceba81f9a6a0661e3212f4612eec246314c295d72da7b50a3f09fafec3"
     sha256 cellar: :any_skip_relocation, mojave:        "bed6d3a0c299e93436cc8aaa93da15484e76a1514bb555fe9eab46ebf51af604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b02f354aa3699f0378e53b17aab83f4a043ed1f32c8e1e9bb7b8e292f126544"
   end
 
   depends_on "go" => :build
