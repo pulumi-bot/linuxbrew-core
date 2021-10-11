a84e3adb491050a69bdec5fd7be3bfdb3b8206e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-11 05:21:28 +0000

    pyright: add 1.1.177 bottle.

--- Formula/pyright.rb
+++ Formula/pyright.rb
@@ -8,6 +8,10 @@ class Pyright < Formula
   license "MIT"
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ac1b4f54fc441828b2897d9bccdf1bb06295d5d29c66b0347320d0cece20474c"
+  end
+
   depends_on "node"
 
   def install
