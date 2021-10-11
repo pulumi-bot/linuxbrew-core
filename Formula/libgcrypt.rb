20b6cc822cb0c609ef64568f56b420a3d885705a
Michka Popoff <michkapopoff@gmail.com>
2021-10-11 18:59:42 +0200

    formulae: add comment to bottle line to generate conflicts on next merge

--- Formula/libgcrypt.rb
+++ Formula/libgcrypt.rb
@@ -16,7 +16,7 @@ class Libgcrypt < Formula
     sha256 cellar: :any,                 big_sur:       "19f11700630c036864c3acaf39d6b26b8d7f46a96b7eab4cab5d118ce5a0c28a"
     sha256 cellar: :any,                 catalina:      "22b69fca91210d5598644b6164980ea3d53ccbb9a66124314ae3836b9100a4bf"
     sha256 cellar: :any,                 mojave:        "d40e101e9605d7ba2b56fa6c441565192a85b3bb67302ab4feeac4d38a56d261"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c199805c55e5f11d84e19554b3583d78a2e681a0ba549508927c2528d07372cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c199805c55e5f11d84e19554b3583d78a2e681a0ba549508927c2528d07372cd" # linuxbrew-core
   end
 
   depends_on "libgpg-error"
