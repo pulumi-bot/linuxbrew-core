7c25a7da0dbcc367ec33c2d6d48a2d639a79db15
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-10-11 09:22:09 +0000

    ttyd: update 1.6.3_4 bottle.

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -12,6 +12,7 @@ class Ttyd < Formula
     sha256 big_sur:       "1d05dbb4c06fdf46c47195d4cbd32939e484fbff6b973ce111166a6fead65b5c"
     sha256 catalina:      "201f490f7ccbb978085190d01585deda06430aada52271cc4ea8f434d16492c6"
     sha256 mojave:        "555135cdd2ff20f6e0c7d95e756e1be4561c458b2f2411c6478b574e726b8b5d"
+    sha256 x86_64_linux:  "b97f691e851cd0d89abdb7297f1177e18600618a8b3d5851e928cd258e89b7f4"
   end
 
   depends_on "cmake" => :build
