aedecaa91b2791e5cbae826b37ed9a129c2cc579
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-14 20:35:39 +0000

    terraform: update 1.0.9 bottle.

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -16,6 +16,7 @@ class Terraform < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e88c4f7892d63ac2b422d9efbd9b37f3126f9449ad5fbcead959e4223e14464c"
     sha256 cellar: :any_skip_relocation, catalina:      "d10c56f76b0eefd04916c832b89af79c2d7ca5bdb942ffd73887c665f7248f2e"
     sha256 cellar: :any_skip_relocation, mojave:        "4c937d477b5b622d162c14a1d9b91e2e8eee2cf182b4627d71138be7aa629a6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f0d2a38b0b567938c8e21528059ec0dbbd36bb14276c5cd98da74bae7c38eb7"
   end
 
   depends_on "go" => :build
