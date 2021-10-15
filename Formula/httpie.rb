aeb6262c7830cd999307575442c03c77dece0cbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-15 20:06:36 +0000

    httpie: update 2.6.0 bottle.

--- Formula/httpie.rb
+++ Formula/httpie.rb
@@ -13,6 +13,7 @@ class Httpie < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "377b0643aa1f6d310ba4cfc70d66a94cc458213db8d134940d3b10a32defacf1"
     sha256 cellar: :any_skip_relocation, catalina:      "6d306c30f6f1d7a551d88415efe12b7c3f25d0602f3579dc632771a463f78fa5"
     sha256 cellar: :any_skip_relocation, mojave:        "f66b8cdff9cb7b44a84197c3e3d81d810f7ff8f2188998b977ccadfc7e2ec893"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d4c49dc9626c33f8a0cb23365636915d4572b417d98f43820eaa564ccf0595d"
   end
 
   depends_on "python@3.10"
