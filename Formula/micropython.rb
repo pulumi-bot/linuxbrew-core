423e57cb128bcf70fae7481b18cdc9649f5042ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-17 20:11:16 +0000

    micropython: update 1.17_2 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -12,6 +12,7 @@ class Micropython < Formula
     sha256 cellar: :any,                 big_sur:       "395c3694341235f8e3eeae947a9bd2692bbebfb7043d4b07f52d3dd211e1da17"
     sha256 cellar: :any,                 catalina:      "ff82df0fd5a96052d8bfc7a0f114fb1f454c742ce6b4aedaebcba62cc24d97a6"
     sha256 cellar: :any,                 mojave:        "8dc695ae73f1c6b8d508ae63af06def4c706d14ca9e007c5b3fce787c7f17ee3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8327f00b5e66af095f7299ae1fa34e9679dc9ae32426b052efa99e080d5db490"
   end
 
   depends_on "pkg-config" => :build
