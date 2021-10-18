c5aefd939ee1ae0b4cc5e6ab004f96e544f2b167
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-18 18:26:47 +0000

    draco: update 1.4.3 bottle.

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -10,6 +10,7 @@ class Draco < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "1290a57ebb6ba3bd53664969513f457c8f85b7c2f543211f08fb72e196abe819"
     sha256 cellar: :any_skip_relocation, catalina:      "5d8c5f99fa4e605adc5268058c31ef8cd109e763e548679e63d6efbb63412941"
     sha256 cellar: :any_skip_relocation, mojave:        "76c996cfd7d4ae8de4fcf9693cc7d1c7e61ea35cb9e730e7e5d005d2b09aa527"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad06047195d285b88d517a76a1253b1b74ff67db4cfe277a4344d4a91e0650dc"
   end
 
   depends_on "cmake" => :build
