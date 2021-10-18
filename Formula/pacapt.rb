bf7e324059fbf446eee56b19925ebdf6c16827ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-18 18:25:26 +0000

    pacapt: add 3.0.5 bottle.

--- Formula/pacapt.rb
+++ Formula/pacapt.rb
@@ -5,6 +5,10 @@ class Pacapt < Formula
   sha256 "c68cd6b8367934f58ab06a131dfbc91ab12ada2baee92ad4246b56729e9cad8d"
   license "Fair"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "66501577cae5a7e78e5dc537df750c883d63fdd6555eef9e4d693b3b264aed71"
+  end
+
   def install
     bin.mkpath
     system "make", "install", "BINDIR=#{bin}", "VERSION=#{version}"
